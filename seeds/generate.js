65df377134a09a6db4960836298502761b847e42
Sebastian Real <seba.real.a@gmail.com>
2023-02-11 15:48:02 -0300

    chore: add new base users to initial seed

--- seeds/generate.js
+++ seeds/generate.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Grades = require('../models/grades');
+const Users = require('../models/users');
 const { store } = require('./util');
 const data = require('./data.json');
 
@@ -8,6 +9,7 @@ mongoose.set('strictQuery', false);
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/civi-conecta-db', {});
   await store(Grades, data.grades);
+  await store(Users, data.users);
   console.log('done');
   process.exit(0);
 }
