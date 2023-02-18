75bb228f17adf28c5285988e7f44f88ed7120ac7
Sebastian Real <seba.real.a@gmail.com>
2023-02-18 18:20:17 -0300

    add migration reset script, its useful in this stage of the project

---
+++ database/reset-migrations.sql
@@ -0,0 +1,19 @@
+
+DROP TABLE IF EXISTS public.user CASCADE;
+DROP TABLE IF EXISTS public.grade CASCADE;
+DROP TABLE IF EXISTS public.letter CASCADE;
+DROP TABLE IF EXISTS public.establishment CASCADE;
+DROP TABLE IF EXISTS public.student CASCADE;
+DROP TABLE IF EXISTS public.topic CASCADE;
+DROP TABLE IF EXISTS public.course CASCADE;
+DROP TABLE IF EXISTS public.course_student CASCADE;
+DROP TABLE IF EXISTS public.unit CASCADE;
+DROP TABLE IF EXISTS public.planning CASCADE;
+DROP TABLE IF EXISTS public.survey CASCADE;
+DROP TABLE IF EXISTS public.question CASCADE;
+DROP TABLE IF EXISTS public.choice CASCADE;
+DROP TABLE IF EXISTS public.event_type CASCADE;
+DROP TABLE IF EXISTS public.event CASCADE;
+
+DROP TABLE IF EXISTS public.knex_migrations CASCADE;
+DROP TABLE IF EXISTS public.knex_migrations_lock CASCADE;
