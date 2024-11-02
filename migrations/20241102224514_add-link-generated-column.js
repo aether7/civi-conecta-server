66cf12b9136b6d12e4b5a1a584128f348543d2a3
winttuu <winttuu@gmail.com>
2024-11-02 19:52:40 -0300

    task: add column is_generated_link in the table 'feedback_course'

---
+++ migrations/20241102224514_add-link-generated-column.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema.alterTable("feedback_course", (table) => {
+    table.integer('is_link_generated').defaultTo(0);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema.alterTable("feedback_course", (table) => {
+    table.dropColumn("is_link_generated");
+  });
+};
