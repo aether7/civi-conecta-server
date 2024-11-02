bb31e450920dad61f3bb30da106d7b3a8f1334ab
winttuu <winttuu@gmail.com>
2024-11-02 14:00:16 -0300

    task EstablishmentTable: add foreign_key `manager_id` that references user

---
+++ migrations/20241102162304_establishment-manager.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('establishment', (table) => {
+    table.integer('manager_id').unsigned().references('id').inTable('user');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+
+};
